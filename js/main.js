aa21569f41c36fc124b70247fb56a9bbc9b3b72d
Agu <agustinpinki11@gmail.com>
2023-09-29 17:33:16 -0300

    JS folder added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log('hello')
+/* console.log("hello world")
+confirm("are you sure you want to download the file?") */
+//this comments just one line
+/* this comments a piece of text*/
+let userAge = prompt("enter your  age")
+if(userAge >= 18){
+    alert("you are an adult")
+}else if (userAge < 18){
+    alert("you are underage")
+}
+
